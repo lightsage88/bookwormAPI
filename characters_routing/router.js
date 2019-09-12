50ed4d5d1901dd4795d0fec6536766dc7ec197a8
Adrian <adrian.e.rosales@gmail.com>
2019-09-11 18:57:40 -0700

    got events working

--- characters_routing/router.js
+++ characters_routing/router.js
@@ -4,6 +4,8 @@ const crypto = require('crypto');
 
 const { mPublicKey, mPrivateKey, mAPI } = require('../config');
 
+const {User} = require('../models');
+
 
 const router = express.Router();
 router.use(express.json());
@@ -40,7 +42,10 @@ router.post('/search', (req,res)=>{
 router.post('/events', (req,res)=>{
     console.log('You are making a POST request to characters/events');
     console.log(req.body);
+    let username = req.body.username;
+    let user;
     let charID = String(req.body.charID);
+    let eventResults;
     console.log(charID);
     let timeStamp = new Date().getTime();
     let hash = require('crypto').createHash('md5').update(timeStamp + mPrivateKey + mPublicKey).digest('hex');
@@ -58,12 +63,49 @@ router.post('/events', (req,res)=>{
     })
     .then(response => {
         console.log(response.data);
-        res.json(response.data.data.results);
+
+        //Need to find the username for the account that initiated this call
+        //Need to find the character for which the call was initiated
+        //Turn the response.data.data.results into what the character's events array is and save it to the User.
+        return response.data.data.results;
+
+        // res.json(response.data.data.results);
     })
+    .then(data => {
+        eventResults = data;
+        console.log('here are the results');
+    console.log(eventResults);
+    })
+    .then(()=>{
+        return User.findOne({"username": username})
+    })
+    .then(_user => {
+        console.log('we should have our guy');
+        user = _user;
+        console.log(user);
+        console.log('fogfogfog');
+        let index = user.characters.findIndex((element) => {
+            return String(element.id) === String(charID);
+        });
+        console.log(index);
+        let revisedCharacter = user.characters.find((element) => {
+            return String(element.id) === String(charID);
+        });
+        
+        revisedCharacter.events = eventResults;
+        console.log(revisedCharacter);
+        let newCharacterArray = user.characters;
+        newCharacterArray[index] = revisedCharacter;
+        user.save();
+        return res.status(201).send(user.characters);
+    })
+
     .catch(err => {
         res.json(err);
         console.error(err);
     });
+    
+
 });
 
 
