b57e246340dc02fadf3daf6d26d597f20a1dba80
Adrian <adrian.e.rosales@gmail.com>
2019-09-18 15:37:34 -0700

    fixed address

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.mPublicKey = process.env.mPublicKey;
 exports.mPrivateKey = process.env.mPrivateKey;
 exports.mAPI = process.env.mAPI;
 // exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/bookwormAPI";
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://lightsage88:<password>@my-first-atlas-db-obkbt.mongodb.net/marvelousBookworm?retryWrites=true&w=majority"
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://lightsage88:Walruses8@my-first-atlas-db-obkbt.mongodb.net/marvelousBookworm?retryWrites=true&w=majority"
 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-bookwormAPI";
 exports.PORT = process.env.PORT || 8000;
