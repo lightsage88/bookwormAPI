5f50b757dc2a8c0b717f88cdd31f9e9c8950e99c
Adrian <adrian.e.rosales@gmail.com>
2019-09-18 12:43:31 -0700

    more things set up

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 exports.mPublicKey = process.env.mPublicKey;
 exports.mPrivateKey = process.env.mPrivateKey;
 exports.mAPI = process.env.mAPI;
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/bookwormAPI";
+// exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/bookwormAPI";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://lightsage88:<password>@my-first-atlas-db-obkbt.mongodb.net/marvelousBookworm?retryWrites=true&w=majority"
+
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-bookwormAPI";
 exports.PORT = process.env.PORT || 8000;
 exports.JWT_SECRET = process.env.JWT_SECRET;
